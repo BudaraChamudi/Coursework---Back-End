f77b9dec2692ecd1d255f8932a8acd9ccec390c2
Budara Chamudi <budara.chamudi@gmail.com>
2024-12-12 23:47:24 +0400

    Add API to Retrieve All Objects from a Collection

--- server.js
+++ server.js
@@ -25,3 +25,9 @@ app.param('collectionName', (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName);
     return next();
 });
+app.get('/collection/:collectionName', (req, res) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
+        res.send(results);
+    });
+});
